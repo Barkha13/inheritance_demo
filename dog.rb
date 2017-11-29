c9b7e73c311f716fed207d4e85c8a0ed13145f16
Barkha13 <barkha0292@yahoo.com>
2017-11-29 15:06:51 -0800

    first commit

---
+++ dog.rb
@@ -0,0 +1,22 @@
+require_relative 'mammal'
+
+class Dog < Mammal
+    def pet
+        @health += 5
+        self
+    end
+
+    def walk
+        @health -= 1
+        self
+    end
+
+    def run
+        @health -=10
+        self
+    end
+
+end
+
+d1 = Dog.new
+d1.walk.walk.walk.run.run.pet.display_health
